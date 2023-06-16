89ddf0de0dcc98544722d9128ccaffa70c6838d5
Sushmitha M Katti <sushmitha.katti.19@gmail.com>
2023-06-16 12:42:26 +0000

    add receptive field formula

--- Session_07-In_Depth_Coding_Practice/models/model3.py
+++ Session_07-In_Depth_Coding_Practice/models/model3.py
@@ -16,24 +16,24 @@ class Model3(nn.Module):
         nn.BatchNorm2d(10),
         nn.Dropout2d(0.02),
 
-        nn.Conv2d(in_channels=10, out_channels=12, kernel_size=(3, 3), padding=0, bias=False), #Op_size = 10, RF = 10   
+        nn.Conv2d(in_channels=10, out_channels=12, kernel_size=(3, 3), padding=0, bias=False), #Op_size = 10, RF = 7  
         nn.ReLU(),
         nn.BatchNorm2d(12),
         nn.Dropout2d(0.02),
 
-        nn.MaxPool2d(kernel_size=(2,2)), #Op_size = 12, RF = 6
+        nn.MaxPool2d(kernel_size=(2,2)), #Op_size = 12, RF = 8
 
-        nn.Conv2d(in_channels=12, out_channels=14, kernel_size=(3, 3), padding=0, bias=False), #Op_size = 8, RF = 14
+        nn.Conv2d(in_channels=12, out_channels=14, kernel_size=(3, 3), padding=0, bias=False), #Op_size = 8, RF = 12
         nn.ReLU(),
         nn.BatchNorm2d(14),
         nn.Dropout2d(0.02),
 
-        nn.Conv2d(in_channels=14, out_channels=14, kernel_size=(3, 3), padding=1, bias=False),#Op_size = 6, RF = 18 
+        nn.Conv2d(in_channels=14, out_channels=14, kernel_size=(3, 3), padding=1, bias=False),#Op_size = 6, RF = 16 
         nn.ReLU(),
         nn.BatchNorm2d(14),
         nn.Dropout(0.02),
 
-        nn.Conv2d(in_channels=14, out_channels=16, kernel_size=(3, 3), padding=0, bias=False),#Op_size = 6, RF = 22 
+        nn.Conv2d(in_channels=14, out_channels=16, kernel_size=(3, 3), padding=0, bias=False),#Op_size = 6, RF = 20 
         nn.ReLU(),
         nn.BatchNorm2d(16),
         nn.Dropout(0.02),
@@ -41,7 +41,7 @@ class Model3(nn.Module):
         nn.AvgPool2d(kernel_size=6),
 
 
-        nn.Conv2d(in_channels=16, out_channels=10, kernel_size=(1, 1), padding=0, bias=False)#Op_size = 1, RF = 28
+        nn.Conv2d(in_channels=16, out_channels=10, kernel_size=(1, 1), padding=0, bias=False)#Op_size = 1, RF = 24
         ) 
 
     def forward(self, x):
