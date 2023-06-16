41f6612ce7bcd62ae43f471a0f4aac1c79388d58
Sushmitha M Katti <sushmitha.katti.19@gmail.com>
2023-06-16 10:33:06 +0530

    Create model7.py

---
+++ Session 7 - In-Depth Coding Practice/models/model7.py
@@ -0,0 +1,50 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Model6(nn.Module):
+    def __init__(self):
+        super(Model6, self).__init__()
+        self.conv1 = nn.Sequential(                      
+            
+        nn.Conv2d(in_channels=1, out_channels=10, kernel_size=(3, 3), padding=0, bias=False),#Op_size = 26, RF = 3
+        nn.ReLU(),
+        nn.BatchNorm2d(10),
+        nn.Dropout2d(0.02),
+
+        nn.Conv2d(in_channels=10, out_channels=10, kernel_size=(3, 3), padding=0, bias=False),#Op_size = 24, RF = 5
+        nn.ReLU(),
+        nn.BatchNorm2d(10),
+        nn.Dropout2d(0.02),
+
+        nn.Conv2d(in_channels=10, out_channels=12, kernel_size=(3, 3), padding=0, bias=False), #Op_size = 10, RF = 10   
+        nn.ReLU(),
+        nn.BatchNorm2d(12),
+        nn.Dropout2d(0.02),
+
+        nn.MaxPool2d(kernel_size=(2,2)), #Op_size = 12, RF = 6
+
+        nn.Conv2d(in_channels=12, out_channels=14, kernel_size=(3, 3), padding=0, bias=False), #Op_size = 8, RF = 14
+        nn.ReLU(),
+        nn.BatchNorm2d(14),
+        nn.Dropout2d(0.02),
+
+        nn.Conv2d(in_channels=14, out_channels=14, kernel_size=(3, 3), padding=1, bias=False),#Op_size = 6, RF = 18 
+        nn.ReLU(),
+        nn.BatchNorm2d(14),
+        nn.Dropout(0.02),
+
+        nn.Conv2d(in_channels=14, out_channels=16, kernel_size=(3, 3), padding=0, bias=False),#Op_size = 6, RF = 22 
+        nn.ReLU(),
+        nn.BatchNorm2d(16),
+        nn.Dropout(0.02),
+
+        nn.AvgPool2d(kernel_size=6),
+
+
+        nn.Conv2d(in_channels=16, out_channels=10, kernel_size=(1, 1), padding=0, bias=False)#Op_size = 1, RF = 28
+        ) 
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = x.view(-1, 10)
+        return F.log_softmax(x, dim=-1)
